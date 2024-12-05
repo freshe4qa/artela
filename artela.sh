0281d2123719eede2f03cd681808526dc9c81137
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2024-12-05 19:28:02 +0200

    Update artela.sh

--- artela.sh
+++ artela.sh
@@ -52,9 +52,9 @@ apt install curl iptables build-essential git wget jq make gcc nano tmux htop nv
 
 # install go
 sudo rm -rf /usr/local/go
-curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
-source .bash_profile
+curl -L https://go.dev/dl/go1.22.7.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.profile
+source .profile
 
 # download binary
 cd $HOME && rm -rf artela
@@ -64,10 +64,10 @@ git checkout v0.4.9-rc9
 make install
 
 #update lib
-cd $HOME
-wget https://github.com/artela-network/artela/releases/download/v0.4.7-rc7-fix-execution/artelad_0.4.7_rc7_fix_execution_Linux_amd64.tar.gz
-tar -xvf artelad_0.4.7_rc7_fix_execution_Linux_amd64.tar.gz
-sudo mv $HOME/libaspect_wasm_instrument.so /usr/lib/
+#cd $HOME
+#wget https://github.com/artela-network/artela/releases/download/v0.4.7-rc7-fix-execution/artelad_0.4.7_rc7_fix_execution_Linux_amd64.tar.gz
+#tar -xvf artelad_0.4.7_rc7_fix_execution_Linux_amd64.tar.gz
+#sudo mv $HOME/libaspect_wasm_instrument.so /usr/lib/
 
 # config
 artelad config chain-id $ARTELA_CHAIN_ID
@@ -77,15 +77,15 @@ artelad config keyring-backend test
 artelad init $NODENAME --chain-id $ARTELA_CHAIN_ID
 
 # download genesis and addrbook
-wget -qO $HOME/.artelad/config/genesis.json https://public-snapshot-storage-develop.s3.ap-southeast-1.amazonaws.com/artela/artela_11822-1/genesis.json
-wget -O $HOME/.artelad/config/addrbook.json https://testnet-files.bonynode.online/artela/addrbook.json
+curl -L https://snapshots-testnet.nodejumper.io/artela/genesis.json > $HOME/.artelad/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/artela/addrbook.json > $HOME/.artelad/config/addrbook.json
 
 # set minimum gas price
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"20000000000uart\"|" $HOME/.artelad/config/app.toml
 
 # set peers and seeds
 SEEDS=""
-PEERS="5c9b1bc492aad27a0197a6d3ea3ec9296504e6fd@artela-testnet-peer.itrocket.net:30656,e7b2f932bf4a58031ce09aab50b80353a4c3b6b4@78.46.46.87:3456,6039d6d5fd29338d5b0f9ccc0182b6ffbb64f618@164.68.111.133:26656,1b0cd81c4b06fd57f613c691dc93bae79b117a89@109.205.182.73:3456,64af6870f342899bfc475da28ce4bb16b0e62f23@161.97.151.149:3456,daf1bfabfd3e0514188659942d854d8d09712986@[2a01:4f8:171:d6e::2]:23456,a9ffffea32e0617f844b2ffbbf5b4548ee94fa44@80.65.211.230:3456,8660f9dd4cd2858b2b5d9a6b726d9d3203625bfc@217.76.48.228:3456,2d48fc88b3313502a1e78fa708ee35cd960bd291@173.249.26.237:30656,4f7b1f100d8f7c78da34ebc4c5978fcccbcebdc7@38.242.252.83:26656,b0120fbe6c7f7bc274472ac2c7176c4fe82582a5@178.18.240.218:3456,55f27297790beba8c0c2a72b412c111ac6dedde1@173.249.29.163:25656,14fbdc03002e348f469dff986c6ca89da3d399c0@38.242.219.113:26656,70ec94e9dd3437f32d78f9f17e0f87278bbaf807@93.189.30.123:26656,8889b28795e8be109a532464e5cc074e113de780@47.251.54.123:26656,830d3e3272bb4a9853ebada3cf2bba27fdcd3ece@47.245.26.161:26656,f809f4fd17a9cf434b059af3e86262bbac3cb809@47.251.32.165:26656,4d108a6199f393ec6018257db306caf9deeda268@95.216.228.91:3456,0172eec239bb213164472ea5cbd96bf07f27d9f2@47.251.14.47:26656"
+PEERS="ff2bfe932a9d4955d5c2c43078a5508a0756e1cd@84.247.168.131:26656,e7cf4a28f99db3899d4b02449e23c9a3e8785dea@213.199.55.29:26656,ea447a8c49aab91971e7a37c7ba56e857048eaa2@77.237.244.71:17656,66bea3849ac05acf61819a2af2c6f4a4f9154625@149.102.137.104:26656,e0c08d7623b2a0dc5d37e01e201055c00fff6b9d@5.189.162.179:45656,21dfb6e516d0977ee89227fc8a626cd8bc5809a9@213.199.45.37:26656,fd08a3be023d7446e3e9aae55e9d6b71e2ce32e2@109.199.124.154:3456,82b42033928b64eaf2229d680d911b7425febdbc@38.242.235.123:26656,ed81be22a54fa50f3598202ddc89050899c61f8d@45.136.17.34:3456,936e6c67633a4bfae82c4f0125824278f827560b@152.53.34.214:3456,64af6870f342899bfc475da28ce4bb16b0e62f23@161.97.151.149:3456,1f09c918f39240cf204996cd1239eccdbb22a779@45.136.17.26:3456,ba19ee8c73b1edbaa54f9114cde15fc2cc37a08e@92.118.58.126:3456,0d6f5be0f227a9dda3c98a1415610caa9e592860@213.199.38.215:45656,0f5a4ad942c2bb222362e7cb92f11f0f474a0f6d@45.136.17.29:3456,b081d81ca2ca19b33f14477694a8a0d4cd444c66@158.51.99.57:26656,3d116e281c93bc73cd35b9036a64c7b3c7a6b1e4@84.46.240.232:3456,9f10c006519ead8942ddbdbcbb5d5f03c2fdf35d@217.76.54.115:26656,0ca7efaf9774d455895a7c845db418435f9272da@195.7.6.216:3456,e28052c540835386e657e9932b46380640014693@213.199.50.196:3456"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.artelad/config/config.toml
 
 # disable indexing
@@ -104,7 +104,7 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $
 sed -i "s/snapshot-interval *=.*/snapshot-interval = 0/g" $HOME/.artelad/config/app.toml
 
 #update
-sed -i -e "s/iavl-disable-fastnode = false/iavl-disable-fastnode = true/" $HOME/.artelad/config/app.toml
+#sed -i -e "s/iavl-disable-fastnode = false/iavl-disable-fastnode = true/" $HOME/.artelad/config/app.toml
 
 # enable prometheus
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.artelad/config/config.toml
@@ -117,7 +117,6 @@ After=network-online.target
 [Service]
 User=$USER
 ExecStart=$(which artelad) start
-Environment="LD_LIBRARY_PATH=/root/libs"
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=65535
@@ -127,7 +126,7 @@ EOF
 
 # reset
 artelad tendermint unsafe-reset-all --home $HOME/.artelad --keep-addr-book
-curl https://server-4.itrocket.net/testnet/artela/artela_2024-09-14_13046951_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.artelad
+curl https://snapshots-testnet.nodejumper.io/artela/artela_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.artelad
 
 # start service
 sudo systemctl daemon-reload
